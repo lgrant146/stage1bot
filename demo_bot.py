65f0879e8ce308353419c51936ab0bd0ef82d638
Leroy Grant <lgrant146@gmail.com>
2020-09-08 23:44:00 -0500

    trying to add reaction to issue post

--- demo_bot.py
+++ demo_bot.py
@@ -8,11 +8,14 @@ async def main():
     async with aiohttp.ClientSession() as session:
         gh = GitHubAPI(session, "lgrant146", oauth_token=os.getenv("GH_AUTH"))
 
-        await gh.post('/repos/lgrant146/stage1bot/issues',
-                      data={
-                          'title': 'Test',
-                          'body': 'We can post issues',
-                      })
+        # await gh.post('/repos/lgrant146/stage1bot/issues',
+        #               data={
+        #                   'title': 'Test',
+        #                   'body': 'Vera is trying to post',
+        #               })
+    # await gh.post('/repos/lgrant146/stage1bot/2/reactions',
+    #               data={"content": "rocket"},
+    #               accept="application/vnd.github.squirrel-girl-preview+json")
 
 
 asyncio.run(main())
